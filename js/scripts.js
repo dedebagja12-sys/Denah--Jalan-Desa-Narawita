c5bdf2f71d9e62dce21e25bbdb317d501a307720
dedebagja12-sys <dedebagja12@gmail.com>
2025-08-11 23:06:03 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,14 @@ window.addEventListener('DOMContentLoaded', event => {
                 navbarToggler.click();
             }
         });
-    });
+    })
 
-});
+document.addEventListener("scroll", function () {
+  const navbar = document.getElementById("mainNav");
+  if (window.scrollY > 50) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+})
+})
